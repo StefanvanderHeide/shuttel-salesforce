9b5453a0e4a77d26f2624663862aae6bfa9505d2
Stefan van der Heide <stefan@MBPvanStefan2.domain_not_set.invalid>
2023-10-25 18:33:29 +0200

    finished toggle setup

--- force-app/main/default/lwc/toggleComponent/toggleComponent.js
+++ force-app/main/default/lwc/toggleComponent/toggleComponent.js
@@ -5,9 +5,20 @@ export default class toggleComponent extends LightningElement {
     @api textProperty;
     @track title;
     @track text;
+    @track showPanel = false;
+    
     
     connectedCallback(){
         this.title = this.titleProperty;
         this.text = this.textProperty;
+    }
+    
+    showPanelFunction(){
+        if(this.showPanel){
+            this.showPanel = false;
+        }else{
+            this.showPanel = true;
+        }
+        console.log(this.showPanel);
     }
 }
