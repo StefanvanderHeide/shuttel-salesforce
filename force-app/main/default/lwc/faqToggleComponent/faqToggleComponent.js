c8dc24cc813d1b14cad4900b934d535c5693feec
Stefan van der Heide <stefan@MacBook-Pro-van-Stefan-2.local>
2023-12-15 09:43:21 +0100

    deployment 1

--- force-app/main/default/lwc/faqToggleComponent/faqToggleComponent.js
+++ force-app/main/default/lwc/faqToggleComponent/faqToggleComponent.js
@@ -9,27 +9,30 @@ export default class faqToggleComponent extends LightningElement {
     @track question;
     @track answer; 
     @track showBottomBorder = true;
+    language;
     
-    @wire(getArticleDetails, {
-        articleNumber: "$articleNumberProperty"
-      })
-      articleDetails({ error, data }) {
-        console.log('bottom border: ' + this.showBottomBorder);
-        if(this.showBottomBorder == 'Ja'){
-          this.showBottomBorder = true;
-        }else{
-          this.showBottomBorder = false;
-        }
-        if (data) {
-          console.log(JSON.stringify(data));
-          this.question = data.question;
-          this.answer = data.answer;
-        } else if (error) {
-          this.error = error;
-          console.log(`Error: ${JSON.stringify(this.error)}`);
-        }
-      }
-    
+    connectedCallback(){
+      this.language = this.getLanguageFromUrl();
+      console.log('language: ' + this.language);
+      getArticleDetails({articleNumber: this.articleNumberProperty, language: this.language}).then(
+        response => {
+          if(this.showBottomBorder == 'Ja'){
+            this.showBottomBorder = true;
+          }else{
+            this.showBottomBorder = false;
+          }
+          console.log(JSON.stringify(response));
+          this.question = response.question;
+          this.answer = response.answer;
+          console.log('question: ' + this.question);
+          console.log('answer: ' + this.answer);
+          }
+      ).catch(error => {
+        this.error = error;
+        console.log(`Error: ${JSON.stringify(this.error)}`);
+     })
+    }
+
     showPanelFunction(){
         if (this.showPanel) {
             this.showPanel = false;
@@ -37,4 +40,10 @@ export default class faqToggleComponent extends LightningElement {
             this.showPanel = true;
         }
     }
+
+    getLanguageFromUrl() {
+      const searchParams = new URLSearchParams(window.location.search);
+      const languageParam = searchParams.get('language');
+      return languageParam;
+  }
 }
