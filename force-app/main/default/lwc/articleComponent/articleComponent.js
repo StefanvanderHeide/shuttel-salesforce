a6f72d3b8891cf3a7c60c9c1db44b39625da08a4
Stefan van der Heide <stefan@MacBook-Pro-van-Stefan-2.local>
2023-12-16 18:55:59 +0100

    article component

--- force-app/main/default/lwc/articleComponent/articleComponent.js
+++ force-app/main/default/lwc/articleComponent/articleComponent.js
@@ -1,5 +1,6 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track} from 'lwc';
 import getArticles from '@salesforce/apex/ArticleComponentController.getArticles';
+import GetResourceURL from '@salesforce/apex/DisplayImageController.getResourceURL'
 import { CurrentPageReference } from 'lightning/navigation';
 
 
@@ -9,14 +10,37 @@ export default class KnowledgeArticles extends LightningElement {
 
     @wire(CurrentPageReference)
     currentPageReference;
-
+    @track noteIcon;
+    @track warningIcon;
+    @track showPanel;
+    @track show1 = true;
+    @track show2 = false;
+    @track show3 = false;
     language;
 
     connectedCallback(){
+
         this.language = this.getLanguageFromUrl();
         getArticles({urlName: this.currentPageReference.attributes.urlName, language: this.language}).then(result => {
             this.articles = result;
+            console.log(JSON.stringify(result));
         });
+        GetResourceURL({resourceName: 'noteIcon'}).then(
+            response => {
+                this.noteIcon = response;
+                console.log(this.noteIcon);
+            }
+        ).catch(error => {
+            console.log('Error: ' + error.body.message);
+        })
+        GetResourceURL({resourceName: 'warningIcon'}).then(
+            response => {
+                this.warningIcon = response;
+                console.log(this.warningIcon);
+            }
+        ).catch(error => {
+            console.log('Error: ' + error.body.message);
+        })
     }
     
     get articlesToDisplay(){
@@ -29,4 +53,26 @@ export default class KnowledgeArticles extends LightningElement {
         return languageParam;
     }
 
+    showPanelFunction(){
+        if (this.showPanel) {
+            this.showPanel = false;
+        } else {
+            this.showPanel = true;
+        }
+    }
+    openTitle1(){
+        this.show1 = true;
+        this.show2 = false;
+        this.show3 = false;
+    }
+    openTitle2(){
+        this.show1 = false;
+        this.show2 = true;
+        this.show3 = false;
+    }
+    openTitle3(){
+        this.show1 = false;
+        this.show2 = false;
+        this.show3 = true;
+    }
 }
